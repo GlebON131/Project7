7e9934d62c7db7bf9eda3788a1258cf48185235e
Gleb Efimov <gleb.efimov@cdpr>
2021-03-19 14:39:59 +0300

    Добавьте файлы проекта.

---
+++ Project7.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+class Animal
+{
+public:
+
+    virtual void Voice() const = 0;
+};
+
+class Dog : public Animal
+{
+    void Voice() const override
+    {
+       cout << "Woof!\n";
+    }
+};
+
+class Cat : public Animal
+{
+    void Voice() const override
+    {
+        cout << "Meaw!\n";
+    }
+};
+
+class Cow : public Animal
+{
+    void Voice() const override
+    {
+        cout << "Moo!\n";
+    }
+};
+
+int main()
+{
+    Animal* animals[3];
+    animals[0] = new Dog();
+    animals[1] = new Cat();
+    animals[2] = new Cow();
+
+    for (Animal* a : animals)
+        a->Voice();
+}
